1f4e8bd8330cf6d9254612ac1618f742e5356787
Noah <98466149+WabosuNopauwaal@users.noreply.github.com>
2023-12-05 14:19:05 +0100

    LAST PUSH
    I did some changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const dropDownMenus = document.querySelectorAll(".drop-down-menu");
 
+  dropDownMenus.forEach(function (menu) {
+    menu.addEventListener("mouseenter", function () {
+      menu.classList.add("open");
+    });
+
+    menu.addEventListener("mouseleave", function () {
+      menu.classList.remove("open");
+    });
+  });
+});
